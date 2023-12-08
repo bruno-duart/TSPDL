47c102b4090f4d1e825f174c74116f6e7ce99821
lcaldeira <caldeira.live@gmail.com>
2023-12-08 16:39:59 -0300

    extern commands moved to globals.h, standard lib includes moved to presets.h

--- lib/globals.h
+++ lib/globals.h
@@ -1,4 +1,9 @@
+#ifndef HEADER_GLOBALS
+#define HEADER_GLOBALS
+
 extern Graph *G;
 extern size_t CONT_GER;
 extern int DIM, PSIZE, MAX_ITER, NUM_TESTES, OPT_VAL;
 extern int *DEMAND, *DRAFT;
+
+#endif
---
+++ lib/presets.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_PRESETS
+#define HEADER_PRESETS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define Type int
+
+#endif
